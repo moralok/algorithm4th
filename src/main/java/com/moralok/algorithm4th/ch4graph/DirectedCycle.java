1aa40bf455511cc54dc8c0fcf34884a45ab7921e
moralok <moralok@163.com>
2020-09-21 21:30:24 +0800

    有向环

---
+++ src/main/java/com/moralok/algorithm4th/ch4graph/DirectedCycle.java
@@ -0,0 +1,63 @@
+package com.moralok.algorithm4th.ch4graph;
+
+import java.util.LinkedList;
+
+/**
+ * 有向环
+ *
+ * @author moralok
+ * @since 2020/9/21
+ */
+public class DirectedCycle {
+
+    private boolean[] marked;
+
+    private int[] edgeTo;
+
+    private boolean[] onStack;
+
+    private LinkedList<Integer> cycle;
+
+    public DirectedCycle(Digraph G) {
+        marked = new boolean[G.V()];
+        onStack = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        for (int i = 0; i < G.V(); i++) {
+            if (!marked[i]) {
+                // 注意未触及的才需要搜索
+                dfs(G, i);
+            }
+        }
+    }
+
+    private void dfs(Digraph G, int v) {
+        marked[v] = true;
+        onStack[v] = true;
+        for (int w : G.adj(v)) {
+            if (hasCycle()) {
+                return;
+            }
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                dfs(G, w);
+            } else if (onStack[w]) {
+                // 如果该点在调用栈上
+                cycle = new LinkedList<Integer>();
+                for (int x = v; x != w; x = edgeTo[x]) {
+                    cycle.push(x);
+                }
+                cycle.push(w);
+                cycle.push(v);
+            }
+        }
+        onStack[v] = false;
+    }
+
+    public boolean hasCycle() {
+        return cycle != null;
+    }
+
+    public Iterable<Integer> cycle() {
+        return cycle;
+    }
+}
